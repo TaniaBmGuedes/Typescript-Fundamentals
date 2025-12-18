74091afde397634fde6d3fc3dbe16aafa1c91848
taniabmguedes@gmail.com <taniabmguedes@gmail.com>
2025-12-18 10:36:26 +0000

    union

--- src/A10-unknown/index.ts
+++ src/A10-unknown/index.ts
@@ -1,1 +1,16 @@
-// Array<T> - T[]
+let x: unknown;
+console.log(x)
+x = 100;
+console.log(x)
+x = 'Tania';
+console.log(x)
+x = 900;
+console.log(x)
+x = 10;
+console.log(x)
+const y = 800;
+console.log(y)
+if (typeof x === 'number') console.log(x + y);
+
+
+// Module mode
