b83d4c35cee5360b0588c8cf95da58fa450be95a
taniabmguedes@gmail.com <taniabmguedes@gmail.com>
2025-12-18 10:31:10 +0000

    enum

--- src/A9-type-enum/index.ts
+++ src/A9-type-enum/index.ts
@@ -1,1 +1,16 @@
-// Array<T> - T[]
+enum Colors {
+  RED = 10,
+  GREEN = 20,
+  BLUE = 50,
+}
+enum Colors {
+  PURPLE = 'P',
+  GREY = 201,
+  YELLOW,
+}
+console.log(Colors);
+
+function chooseColor(color: Colors): void {
+  console.log(color);
+}
+chooseColor(Colors.BLUE);
